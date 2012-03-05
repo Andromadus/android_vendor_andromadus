e7abe8ef6afa9e50fc1f326088cd12be76d6fa15
Nicholas Flintham <nick@flinny.org>
2012-03-05 12:48:59 +0000

    Initial Commit for Andromadus Repo, GooManager config

---
+++ config/andromadus.mk
@@ -0,0 +1,8 @@
+# GSM APN list
+PRODUCT_COPY_FILES += \
+    vendor/andromadus/prebuilt/GooManager.apk:system/bin/GooManager.apk
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.goo.developerid=andromadus \
+    ro.goo.rom=Andromadus \
+    ro.goo.version=13
