b6c828d0779e5062c24346c36e74c5fd51b15a2b
Nicholas Flintham <nick@flinny.org>
2012-03-08 12:32:17 +0000

    Changed Andromadus Version and Goomanager version to date string and update Goo manager

--- config/andromadus.mk
+++ config/andromadus.mk
@@ -3,6 +3,7 @@ PRODUCT_COPY_FILES += \
     vendor/andromadus/prebuilt/GooManager.apk:system/app/GooManager.apk
 
 PRODUCT_PROPERTY_OVERRIDES += \
+    ro.modversion=$(shell date +%s) \
     ro.goo.developerid=andromadus \
     ro.goo.rom=Andromadus \
-    ro.goo.version=13
+    ro.goo.version=$(shell date +%s)
