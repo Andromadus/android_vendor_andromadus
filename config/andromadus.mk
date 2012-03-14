581c77997f19ee32473244222102e5e0321176dd
darkspadez <darkspadez@gmail.com>
2012-03-13 23:28:31 -0400

    Use already prebuilt GooManager from GooManager Devs instead of constantly updating andromadus vendor.

--- config/andromadus.mk
+++ config/andromadus.mk
@@ -1,9 +1,9 @@
-# GSM APN list
-PRODUCT_COPY_FILES += \
-    vendor/andromadus/prebuilt/GooManager.apk:system/app/GooManager.apk
+PRODUCT_PACKAGES += \
+    GooManager
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(shell date +%s) \
     ro.goo.developerid=andromadus \
     ro.goo.rom=Andromadus \
-    ro.goo.version=$(shell date +%s)
+    ro.goo.version=$(shell date +%s) \
+    ro.goo.board=vision
