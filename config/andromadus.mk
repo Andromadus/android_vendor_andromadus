9a7ee207b7271bb52002c101f4379046320afda2
Jon Kagan <darkspadez@gmail.com>
2012-03-30 13:54:53 -0300

    Update config/andromadus.mk

--- config/andromadus.mk
+++ config/andromadus.mk
@@ -1,9 +1,9 @@
 PRODUCT_PACKAGES += \
-    GooManager
+    GooManager \
+    FM
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(shell date +%s) \
     ro.goo.developerid=andromadus \
     ro.goo.rom=Andromadus \
-    ro.goo.version=$(shell date +%s) \
-    ro.goo.board=vision
+    ro.goo.version=$(shell date +%s)
