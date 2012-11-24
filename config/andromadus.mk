31aede755bf233b3ccbae78a7c94b549a684f785
Nicholas Flintham <nick@flinny.org>
2012-11-24 08:47:20 +0000

    Update .mk and add CM's apns

--- config/andromadus.mk
+++ config/andromadus.mk
@@ -3,4 +3,7 @@ PRODUCT_PACKAGES += \
     su
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(shell date +%s) \
+    ro.modversion=$(shell date +%s)
+
+PRODUCT_COPY_FILES += \
+    vendor/andromadus/apns-conf.xml:system/etc/apns-conf.xml
