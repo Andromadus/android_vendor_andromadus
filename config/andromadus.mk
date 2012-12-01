8a24409fa3aadac65194c165e7dca4f6f6f353d8
Nicholas Flintham <nick@flinny.org>
2012-12-01 17:38:06 +0000

    backuptool stuff

--- config/andromadus.mk
+++ config/andromadus.mk
@@ -5,7 +5,12 @@ PRODUCT_PACKAGES += \
     Stk
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(shell date +%s)
+    ro.modversion=$(shell date +%d%m%Y)
 
 PRODUCT_COPY_FILES += \
     vendor/andromadus/apns-conf.xml:system/etc/apns-conf.xml
+
+# Backup Tool
+PRODUCT_COPY_FILES += \
+    vendor/andromadus/tools/backuptool.sh:system/bin/backuptool.sh \
+    vendor/andromadus/tools/backuptool.functions:system/bin/backuptool.functions
